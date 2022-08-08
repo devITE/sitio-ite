6c3258744764cf78ea59b0830e7211993887914f
SoporteITE <soporte@itetlax.org.mx>
2022-08-08 16:57:28 -0500

    cambios

--- src/pages/acercaITE/FinesInstitucionales.js
+++ src/pages/acercaITE/FinesInstitucionales.js
@@ -4,7 +4,7 @@ export default class FinesInstitucionales extends Component {
   render() {
     return (
       <div>
-        <h1>Fines Institucionales</h1>
+        <h1>Fines Institucionales 4</h1>
       </div>
     );
   }
