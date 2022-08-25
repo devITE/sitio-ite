08fa126536a05e373b5438fe3423245d4e30029b
InformaticaITE <informatica@itetlax.org.mx>
2022-08-25 12:58:54 -0500

    add styles to caruselTopIndex

--- src/assets/dataCarousel/dataCarouselIndex.js
+++ src/assets/dataCarousel/dataCarouselIndex.js
@@ -1,69 +1,69 @@
 const Carousel__imgpath = require.context("../../img/imgC_I");
-
-// import moduleName from '../../img'
+const Carousel__pdfpath = require.context("../pdf");
+const Carousel__wordpath = require.context("../word");
 
 export const imgCarouselIndexTop = [
   {
     id: 0,
-    url: Carousel__imgpath(`./${"1.jpg"}`),
+    url: Carousel__imgpath(`./${"0.jpg"}`),
     alt: "Agenda de Asambleas",
     itemActive: "active",
     buttonActive: "active",
     ariaCurrent: "true",
-    //     link: Carousel__pdfpath(`./${"1.pdf"}`),
+    link: Carousel__pdfpath(`./${"0.pdf"}`),
   },
   {
     id: 1,
-    url: Carousel__imgpath(`./${"2.jpg"}`),
+    url: Carousel__imgpath(`./${"1.jpg"}`),
     alt: "Verificación de afiliación",
-    //     link: "https://deppp-partidos.ine.mx/afiliadosPartidos/app/publico/consultaAfiliados/nacionales?execution=e1s1",
+    link: "https://deppp-partidos.ine.mx/afiliadosPartidos/app/publico/consultaAfiliados/nacionales?execution=e1s1",
   },
   {
     id: 2,
-    url: Carousel__imgpath(`./${"3.png"}`),
+    url: Carousel__imgpath(`./${"2.png"}`),
     alt: "Quinto concurso nacional",
-    //     link: "https://www.ceenl.mx/2022/cineminuto/",
+    link: "https://www.ceenl.mx/2022/cineminuto/",
   },
   {
     id: 3,
-    url: Carousel__imgpath(`./${"4.png"}`),
+    url: Carousel__imgpath(`./${"3.png"}`),
     alt: "Concurso Ensayo",
-    //     link: Carousel__pdfpath(`./${"4.pdf"}`),
+    link: Carousel__pdfpath(`./${"3.pdf"}`),
   },
   {
     id: 4,
-    url: Carousel__imgpath(`./${"5.jpg"}`),
+    url: Carousel__imgpath(`./${"4.jpg"}`),
     alt: "Convocatoria Consejero Presidente(a)",
-    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+    link: Carousel__pdfpath(`./${"4.pdf"}`),
   },
   {
     id: 5,
-    url: Carousel__imgpath(`./${"6.jpg"}`),
+    url: Carousel__imgpath(`./${"5.jpg"}`),
     alt: "Formatos de fiscalización editables",
-    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+    link: Carousel__wordpath(`./${"5.docx"}`),
   },
   {
     id: 6,
-    url: Carousel__imgpath(`./${"7.jpg"}`),
+    url: Carousel__imgpath(`./${"6.jpg"}`),
     alt: "Guia de actuación",
-    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+    link: Carousel__pdfpath(`./${"6.pdf"}`),
   },
   {
     id: 7,
-    url: Carousel__imgpath(`./${"8.jpg"}`),
+    url: Carousel__imgpath(`./${"7.jpg"}`),
     alt: "Manual android",
-    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+    link: Carousel__pdfpath(`./${"7.pdf"}`),
   },
   {
     id: 8,
-    url: Carousel__imgpath(`./${"9.jpg"}`),
+    url: Carousel__imgpath(`./${"8.jpg"}`),
     alt: "Manual ios",
-    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+    link: Carousel__pdfpath(`./${"8.pdf"}`),
   },
   {
     id: 9,
-    url: Carousel__imgpath(`./${"10.jpg"}`),
+    url: Carousel__imgpath(`./${"9.jpg"}`),
     alt: "Manual web",
-    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+    link: Carousel__pdfpath(`./${"9.pdf"}`),
   },
 ];
