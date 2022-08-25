5638594f20aa56c5bf05f2f0d8985951c415f644
InformaticaITE <informatica@itetlax.org.mx>
2022-08-25 12:09:26 -0500

    add file data

---
+++ src/assets/dataCarousel/dataCarouselIndex.js
@@ -0,0 +1,69 @@
+const Carousel__imgpath = require.context("../../img/imgC_I");
+
+// import moduleName from '../../img'
+
+export const imgCarouselIndexTop = [
+  {
+    id: 0,
+    url: Carousel__imgpath(`./${"1.jpg"}`),
+    alt: "Agenda de Asambleas",
+    itemActive: "active",
+    buttonActive: "active",
+    ariaCurrent: "true",
+    //     link: Carousel__pdfpath(`./${"1.pdf"}`),
+  },
+  {
+    id: 1,
+    url: Carousel__imgpath(`./${"2.jpg"}`),
+    alt: "Verificación de afiliación",
+    //     link: "https://deppp-partidos.ine.mx/afiliadosPartidos/app/publico/consultaAfiliados/nacionales?execution=e1s1",
+  },
+  {
+    id: 2,
+    url: Carousel__imgpath(`./${"3.png"}`),
+    alt: "Quinto concurso nacional",
+    //     link: "https://www.ceenl.mx/2022/cineminuto/",
+  },
+  {
+    id: 3,
+    url: Carousel__imgpath(`./${"4.png"}`),
+    alt: "Concurso Ensayo",
+    //     link: Carousel__pdfpath(`./${"4.pdf"}`),
+  },
+  {
+    id: 4,
+    url: Carousel__imgpath(`./${"5.jpg"}`),
+    alt: "Convocatoria Consejero Presidente(a)",
+    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+  },
+  {
+    id: 5,
+    url: Carousel__imgpath(`./${"6.jpg"}`),
+    alt: "Formatos de fiscalización editables",
+    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+  },
+  {
+    id: 6,
+    url: Carousel__imgpath(`./${"7.jpg"}`),
+    alt: "Guia de actuación",
+    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+  },
+  {
+    id: 7,
+    url: Carousel__imgpath(`./${"8.jpg"}`),
+    alt: "Manual android",
+    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+  },
+  {
+    id: 8,
+    url: Carousel__imgpath(`./${"9.jpg"}`),
+    alt: "Manual ios",
+    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+  },
+  {
+    id: 9,
+    url: Carousel__imgpath(`./${"10.jpg"}`),
+    alt: "Manual web",
+    //     link: Carousel__pdfpath(`./${"5.pdf"}`),
+  },
+];
