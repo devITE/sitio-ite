07d3c361faa07f065df84e7442ccf314bc1a4a1e
InformaticaITE <informatica@itetlax.org.mx>
2022-09-12 17:09:11 -0500

    add map to informes

--- src/pages/index/informes/Informes.js
+++ src/pages/index/informes/Informes.js
@@ -3,11 +3,13 @@ const Informes = ({ items }) => {
     <div className="w-100 Home__informes ps-5 pe-5">
       {items.map((items) => (
         <div key={items.id}>
-          <img
-            src={items.url}
-            className="img-fluid rounded-3 shadow-sm"
-            alt={items.alt}
-          />
+          <a href={items.link} target="_blank" rel="noreferrer">
+            <img
+              src={items.url}
+              className="img-fluid rounded-3 shadow-sm"
+              alt={items.alt}
+            />
+          </a>
         </div>
       ))}
     </div>
