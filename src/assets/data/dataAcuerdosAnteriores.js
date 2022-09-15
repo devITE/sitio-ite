bd770579055a1fcff287fd042150ba5c035b6ad4
InformaticaITE <informatica@itetlax.org.mx>
2022-09-15 11:19:41 -0500

    create Acuerdos 2015

---
+++ src/assets/data/dataAcuerdosAnteriores.js
@@ -0,0 +1,12 @@
+// const Estrados__pdfpath = require.context("../pdf/a");
+
+export const dataAcuerdos2015 = [
+  {
+    id: 0,
+    year: "2015",
+    month: "Enero",
+    name: "CG 01-2015 9-ENERO-2015 ACUERDO INTEGRACIÓN DE JUNTA GENERAL EJECUTIVA",
+    // link: Estrados__pdfpath(`./${"a2015/"}${"0.pdf"}`),
+    link: "hola",
+  },
+];
--- src/pages/acuerdos/Acuerdos2015.js
+++ src/pages/acuerdos/Acuerdos2015.js
@@ -1,10 +1,38 @@
 import React, { Component } from "react";
+import { dataAcuerdos2015 } from "../../assets/data/dataAcuerdosAnteriores";
+import top2015 from "../../img/ac/2015.png";
 
-export default class acuerdos2015 extends Component {
+export default class Acuerdos2015 extends Component {
   render() {
     return (
       <div>
-        <h1>Acuerdos2015</h1>
+        <img
+          src={top2015}
+          alt="Acuerdos 2015"
+          className="img-fluid w-100 mb-3"
+        />
+        <table class="table table-hover table-responsive">
+          <thead className="table-light">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Mes</th>
+              <th scope="col">Acuerdo</th>
+              <th scope="col">File</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataAcuerdos2015.map((acuerdo) => {
+              return (
+                <tr key={acuerdo.id}>
+                  <th scope="row">{acuerdo.id}</th>
+                  <td>{acuerdo.month}</td>
+                  <td>{acuerdo.name}</td>
+                  <td>{acuerdo.link}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
       </div>
     );
   }
