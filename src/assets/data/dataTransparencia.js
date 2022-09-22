1e92bcca6e38866691316fab03436eda9e15ab79
InformaticaITE <informatica@itetlax.org.mx>
2022-09-22 14:17:05 -0500

    add datatable prev

---
+++ src/assets/data/dataTransparencia.js
@@ -0,0 +1,133 @@
+export const dataArt632017 = [
+  {
+    id: 1,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 2,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 3,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 4,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 5,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 6,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 7,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 8,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 9,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+  {
+    id: 10,
+    fraccion: "I",
+    titulo: "Marco Normativo Aplicable de Sujeto Obligado",
+    cumplimiento: "Aplica",
+    fundamentoLegal:
+      "Articulo. 63 Fraccion I de la Ley de Transparencia y Acceso a la Información Pública del Estado de Tlaxcala",
+    actualizacion: "2022",
+    file: "hola",
+  },
+];
+
+export const columnsArt632017 = [
+  {
+    name: "No.",
+    selector: (row) => row.id,
+  },
+  {
+    name: "Fracción",
+    selector: (row) => row.fraccion,
+  },
+  {
+    name: "Título",
+    selector: (row) => row.titulo,
+  },
+  {
+    name: "Cumplimiento",
+    selector: (row) => row.cumplimiento,
+  },
+  {
+    name: "Fundamento Legal",
+    selector: (row) => row.fundamentoLegal,
+  },
+  {
+    name: "Actualización",
+    selector: (row) => row.actualizacion,
+  },
+  {
+    name: "Excel",
+    selector: (row) => row.f,
+  },
+];
--- src/pages/transparencia/art63/Art632017.js
+++ src/pages/transparencia/art63/Art632017.js
@@ -1,25 +1,103 @@
-import React, { Component } from "react";
+import React, { useMemo } from "react";
+import MaterialReactTable from "material-react-table";
 import "../Transparencia.css";
 
 const Transparencia_imppath = require.context("../../../img/imgT");
 
-export default class Art632017 extends Component {
-  render() {
-    return (
-      <div>
-        <img
-          className="img-fluid w-100"
-          src={Transparencia_imppath(`./head.png`)}
-          alt="Transparencia"
-        />
-        <div className="w-100 mt-3 Transparencia__header-title">
-          Ley de Transparencia y Acceso a la Información Pública del Estado de
-          Tlaxcala. 2016.
-        </div>
-        <h5 className="mt-3 Transparencia__title">
-          Artículo 63. (2022) Obligaciones Comunes
-        </h5>
+//nested data is ok, see accessorKeys in ColumnDef below
+const data = [
+  {
+    name: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    address: "261 Erdman Ford",
+    city: "East Daphne",
+    state: "Kentucky",
+  },
+  {
+    name: {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    address: "769 Dominic Grove",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Joe",
+      lastName: "Doe",
+    },
+    address: "566 Brakus Inlet",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Vandy",
+    },
+    address: "722 Emie Stream",
+    city: "Lincoln",
+    state: "Nebraska",
+  },
+  {
+    name: {
+      firstName: "Joshua",
+      lastName: "Rolluffs",
+    },
+    address: "32188 Larkin Turnpike",
+    city: "Charleston",
+    state: "South Carolina",
+  },
+];
+
+const Art632017 = () => {
+  //should be memoized or stable
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "First Name",
+      },
+      {
+        accessorKey: "name.lastName",
+        header: "Last Name",
+      },
+      {
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
+      },
+      {
+        accessorKey: "city",
+        header: "City",
+      },
+      {
+        accessorKey: "state",
+        header: "State",
+      },
+    ],
+    []
+  );
+
+  return (
+    <div>
+      <img
+        className="img-fluid w-100"
+        src={Transparencia_imppath(`./head.png`)}
+        alt="Transparencia"
+      />
+      <div className="w-100 mt-3 Transparencia__header-title">
+        Ley de Transparencia y Acceso a la Información Pública del Estado de
+        Tlaxcala. 2016.
       </div>
-    );
-  }
-}
+      <h5 className="mt-3 Transparencia__title">
+        Artículo 63. (2022) Obligaciones Comunes
+      </h5>
+      <MaterialReactTable columns={columns} data={data} />
+    </div>
+  );
+};
+
+export default Art632017;
