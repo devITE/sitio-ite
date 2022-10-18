9b7d6854527ee49e5901912b2a7c7952f7f513f3
InformaticaITE <informatica@itetlax.org.mx>
2022-10-18 16:59:59 -0500

    add Modal Component

--- src/pages/acuerdos/AcuerdosAnteriores.js
+++ src/pages/acuerdos/AcuerdosAnteriores.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
 import "./Acuerdos.css";
 
 const AcuerdosAnteriores__imgpath = require.context("../../img/imgA/");
@@ -14,11 +13,11 @@ export default class AcuerdosAnteriores extends Component {
           src={AcuerdosAnteriores__imgpath(`./AA.png`)}
           alt="Acuerdos Anteriores"
         />
-        <div className="w-100 mt-5 AcuerdosITE">
+        <div className="w-100 mt-5 Acuerdos">
           <div>
             <Link to="../../Acuerdos2015">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2015.png`)}
                 alt="Acuerdos 2015"
               />
@@ -27,7 +26,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2014" className="nav-link disabled">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2014.png`)}
                 alt="Acuerdos 2014"
               />
@@ -36,7 +35,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2013" className="nav-link disabled">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2013.png`)}
                 alt="Acuerdos 2013"
               />
@@ -45,7 +44,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2012" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2012.png`)}
                 alt="Acuerdos 2012"
               />
@@ -54,7 +53,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2011" className="nav-link disabled">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2011.png`)}
                 alt="Acuerdos 2011"
               />
@@ -63,7 +62,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2010" className="nav-link disabled">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2010.png`)}
                 alt="Acuerdos 2010"
               />
@@ -72,7 +71,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2009" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2009.png`)}
                 alt="Acuerdos 2009"
               />
@@ -81,7 +80,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2008" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2008.png`)}
                 alt="Acuerdos 2008"
               />
@@ -90,7 +89,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2007" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2007.png`)}
                 alt="Acuerdos 2007"
               />
@@ -99,7 +98,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2006" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2006.png`)}
                 alt="Acuerdos 2006"
               />
@@ -108,7 +107,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2005" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2005.png`)}
                 alt="Acuerdos 2005"
               />
@@ -117,7 +116,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2004" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2004.png`)}
                 alt="Acuerdos 2004"
               />
@@ -126,7 +125,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2003" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2003.png`)}
                 alt="Acuerdos 2003"
               />
@@ -135,7 +134,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2002" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2002.png`)}
                 alt="Acuerdos 2002"
               />
@@ -144,7 +143,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos2001" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./2001.png`)}
                 alt="Acuerdos 2001"
               />
@@ -153,7 +152,7 @@ export default class AcuerdosAnteriores extends Component {
           <div>
             <Link to="../../Acuerdos1998" className="nav-link">
               <img
-                className="img-fluid AcuerdosITE_link"
+                className="img-fluid Acuerdos_link"
                 src={AcuerdosAnteriores__imgpath(`./1998.png`)}
                 alt="Acuerdos 1998"
               />
--- src/pages/acuerdos/acuerdosINE/AcuerdosINE.js
+++ src/pages/acuerdos/acuerdosINE/AcuerdosINE.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Modal from "../../../layout/Modal";
 
 import "../Acuerdos.css";
 
@@ -14,11 +15,24 @@ export default class AcuerdosINE extends Component {
           src={Acuerdos__imgpath(`./AINE.png`)}
           alt="Acuerdos INE"
         />
-        <div className="w-100 mt-5 AcuerdosAnteriores">
+        <button
+          type="button"
+          class="btn btn-primary"
+          data-bs-toggle="modal"
+          data-bs-target="#modalEjemplo"
+        >
+          Launch demo modal
+        </button>
+        <Modal
+          idModal="modalEjemplo"
+          titleModal="Modal EJEMPLO"
+          contentModal="Contenido Modal"
+        />
+        <div className="w-100 mt-5 Acuerdos">
           <div>
             <Link to="../../AcuerdosINE2015">
               <img
-                className="img-fluid AcuerdosAnteriores_link"
+                className="img-fluid Acuerdos_link"
                 src={Acuerdos__imgpath(`./2015.png`)}
                 alt="Acuerdos INE 2015"
               />
@@ -27,7 +41,7 @@ export default class AcuerdosINE extends Component {
           <div>
             <Link to="../../AcuerdosINE2016" className="nav-link">
               <img
-                className="img-fluid AcuerdosAnteriores_link"
+                className="img-fluid Acuerdos_link"
                 src={Acuerdos__imgpath(`./2016.png`)}
                 alt="Acuerdos INE 2016"
               />
@@ -36,7 +50,7 @@ export default class AcuerdosINE extends Component {
           <div>
             <Link to="../../AcuerdosINE2020" className="nav-link">
               <img
-                className="img-fluid AcuerdosAnteriores_link"
+                className="img-fluid Acuerdos_link"
                 src={Acuerdos__imgpath(`./2020.png`)}
                 alt="Acuerdos INE 2020"
               />
@@ -45,7 +59,7 @@ export default class AcuerdosINE extends Component {
           <div>
             <Link to="../../AcuerdosINE2021" className="nav-link">
               <img
-                className="img-fluid AcuerdosAnteriores_link"
+                className="img-fluid Acuerdos_link"
                 src={Acuerdos__imgpath(`./2021.png`)}
                 alt="Acuerdos INE 2021"
               />
@@ -54,7 +68,7 @@ export default class AcuerdosINE extends Component {
           <div>
             <Link to="../../AcuerdosINE2022" className="nav-link">
               <img
-                className="img-fluid AcuerdosAnteriores_link"
+                className="img-fluid Acuerdos_link"
                 src={Acuerdos__imgpath(`./2022.png`)}
                 alt="Acuerdos INE 2022"
               />
@@ -63,7 +77,7 @@ export default class AcuerdosINE extends Component {
           <div>
             <Link to="../../AcuerdosINE2023" className="nav-link">
               <img
-                className="img-fluid AcuerdosAnteriores_link"
+                className="img-fluid Acuerdos_link"
                 src={Acuerdos__imgpath(`./2023.png`)}
                 alt="Acuerdos INE 2023"
               />
