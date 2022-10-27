45f976b555f07a9937a6ae19f8b04d65143e72a3
PalomaRmMz <p.ramirez.m.8080@gmail.com>
2022-10-26 22:31:20 -0500

    add some img

--- src/assets/data/dataAccordion.js
+++ src/assets/data/dataAccordion.js
@@ -21,6 +21,48 @@ export const dataMenuInf = [
           },
         ],
       },
+      {
+        id: 2,
+        titleButton: "Febrero",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Constitución de Partidos Políticos Locales",
+            link: Infografias__imgpath(`./${"2022/"}${"3.jpg"}`),
+          },
+          {
+            id: 2,
+            alt: "Plebiscito 1",
+            link: Infografias__imgpath(`./${"2022/"}${"4.jpeg"}`),
+          },
+          {
+            id: 3,
+            alt: "Plebiscito 2",
+            link: Infografias__imgpath(`./${"2022/"}${"5.jpeg"}`),
+          },
+          {
+            id: 4,
+            alt: "¿Como nos identificamos'",
+            link: Infografias__imgpath(`./${"2022/"}${"6.jpeg"}`),
+          },
+          {
+            id: 5,
+            alt: "Infografía 1",
+            link: Infografias__imgpath(`./${"2022/"}${"7.jpeg"}`),
+          },
+        ],
+      },
+      {
+        id: 3,
+        titleButton: "Marzo",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Sesiones de Consejo General",
+            link: Infografias__imgpath(`./${"2022/"}${"8.jpg"}`),
+          },
+        ],
+      },
     ],
   },
   {
@@ -33,8 +75,8 @@ export const dataMenuInf = [
         subChildren: [
           {
             id: 1,
-            alt: "InfografiaALT2",
-            link: "InfografiaLINK2",
+            alt: "Transparencia",
+            link: Infografias__imgpath(`./${"2021/"}${"1.jpg"}`),
           },
         ],
       },
@@ -44,9 +86,109 @@ export const dataMenuInf = [
         subChildren: [
           {
             id: 1,
-            titleButton: "Febrero",
-            alt: "InfografiaALT2",
-            link: "InfografiaLINK2",
+            alt: "Dirección de Asuntos Jurídicos",
+            link: Infografias__imgpath(`./${"2021/"}${"2.jpg"}`),
+          },
+          {
+            id: 2,
+            alt: "Infografía 1",
+            link: Infografias__imgpath(`./${"2021/"}${"3.jpg"}`),
+          },
+        ],
+      },
+      {
+        id: 3,
+        titleButton: "Marzo",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Infografía 1",
+            link: Infografias__imgpath(`./${"2021/"}${"4.jpg"}`),
+          },
+          {
+            id: 2,
+            alt: "Órgano Interno de Control",
+            link: Infografias__imgpath(`./${"2021/"}${"5.jpeg"}`),
+          },
+        ],
+      },
+      {
+        id: 4,
+        titleButton: "Abril",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Derechos ARCO",
+            link: Infografias__imgpath(`./${"2021/"}${"6.jpg"}`),
+          },
+          {
+            id: 2,
+            alt: "Medidas sanitarias para el voto seguro",
+            link: Infografias__imgpath(`./${"2021/"}${"7.jpg"}`),
+          },
+        ],
+      },
+      {
+        id: 5,
+        titleButton: "Mayo",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Prep",
+            link: Infografias__imgpath(`./${"2021/"}${"8.jpg"}`),
+          },
+          {
+            id: 2,
+            alt: "Referendum",
+            link: Infografias__imgpath(`./${"2021/"}${"9.jpeg"}`),
+          },
+        ],
+      },
+      {
+        id: 6,
+        titleButton: "Junio",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Proceso Electoral Local Ordinario ",
+            link: Infografias__imgpath(`./${"2021/"}${"10.jpeg"}`),
+          },
+          {
+            id: 2,
+            alt: "Referendum",
+            link: Infografias__imgpath(`./${"2021/"}${"11.jpeg"}`),
+          },
+        ],
+      },
+      {
+        id: 7,
+        titleButton: "Julio",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Proceso Electoral Local Ordinario 2020-2021 Medios de Comunicación",
+            link: Infografias__imgpath(`./${"2021/"}${"12.png"}`),
+          },
+          {
+            id: 2,
+            alt: "Plebiscito",
+            link: Infografias__imgpath(`./${"2021/"}${"13.png"}`),
+          },
+        ],
+      },
+      {
+        id: 8,
+        titleButton: "Agosto",
+        subChildren: [
+          {
+            id: 1,
+            alt: "Financiamiento Público",
+            link: Infografias__imgpath(`./${"2021/"}${"14.jpg"}`),
+          },
+          {
+            id: 2,
+            alt: "Plebiscito",
+            link: Infografias__imgpath(`./${"2021/"}${"15.png"}`),
           },
         ],
       },
