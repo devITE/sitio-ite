a509975e05db2b5aa18651988ac6374f463facd8
SoporteITE <soporte@itetlax.org.mx>
2023-04-05 09:52:23 -0600

    actualizaciones estrados electronicos

--- src/data/dataEstradosConvocatorias.js
+++ src/data/dataEstradosConvocatorias.js
@@ -6,13 +6,21 @@ export const dataEstrados = [
   //   nameFolio: "FOLIO 3211-2022",
   //   linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/1.1.pdf",
   // },
-//  {
-//   id: 1,
-//   nameCedula: "CÉDULA DE PUBLICITACIÓN FOLIO 0317-2023",
-//   linkCedula: "https://itetlax.org.mx/assets/pdf/estrados/4.pdf",
-//   nameFolio: "FOLIO 0317-2023",
-//   linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/4.1.pdf",
-// },
+  {
+   id: 1,
+   nameCedula: "CÉDULA DE PUBLICITACIÓN FOLIO 0676-2023",
+   linkCedula: "https://itetlax.org.mx/assets/pdf/estrados/5.pdf",
+   nameFolio: "FOLIO 0676-2023",
+   linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/5.1.pdf",
+ },
+ {
+  id: 2,
+  nameCedula: "CÉDULA DE PUBLICITACIÓN FOLIO 0677-2023",
+  linkCedula: "https://itetlax.org.mx/assets/pdf/estrados/6.pdf",
+  nameFolio: "FOLIO 0677-2023",
+  linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/6.1.pdf",
+},
+//Fechas de baja el 11 de abril de ambas cedulas
 ];
 
 
