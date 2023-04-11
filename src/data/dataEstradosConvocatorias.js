b9975212e93e3097663da86097569ff0e78f9717
informaticaITE <informatica@itetlax.org.mx>
2023-04-11 17:42:36 -0600

    Cedula de publicitacion folio 0991-2023 11/04/2023 PRM

--- src/data/dataEstradosConvocatorias.js
+++ src/data/dataEstradosConvocatorias.js
@@ -7,14 +7,20 @@ export const dataEstrados = [
   //   linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/1.1.pdf",
   // },
   {
-   id: 1,
-   nameCedula: "CÉDULA DE PUBLICITACIÓN FOLIO 0922",
-   linkCedula: "https://itetlax.org.mx/assets/pdf/estrados/7.pdf",
-   nameFolio: "FOLIO 0922-2023",
-   linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/7.1.pdf",
- },
+    id: 1,
+    nameCedula: "CÉDULA DE PUBLICITACIÓN FOLIO 0922",
+    linkCedula: "https://itetlax.org.mx/assets/pdf/estrados/7.pdf",
+    nameFolio: "FOLIO 0922-2023",
+    linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/7.1.pdf",
+  },
+  {
+    id: 2,
+    nameCedula: "CÉDULA DE PUBLICITACIÓN FOLIO 0991",
+    linkCedula: "https://itetlax.org.mx/assets/pdf/estrados/8.pdf",
+    nameFolio: "FOLIO 0991-2023",
+    linkFolio: "https://itetlax.org.mx/assets/pdf/estrados/8.1.pdf",
+  },
 ];
-
 
 export const dataConvocatorias = [
   // {
@@ -22,5 +28,4 @@ export const dataConvocatorias = [
   //   nameFile: "CÉDULA DE PUBLICITACIÓN FOLIO 3211",
   //   linkFile: "https://itetlax.org.mx/assets/pdf/estrados/1.pdf",
   // },
-   
 ];
