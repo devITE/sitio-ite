ec14097e3ebaefbda21481bb01cb592cd9487ed2
PalomaRmMz <p.ramirez.m.8080@gmail.com>
2024-01-09 11:12:04 -0600

    acuerdo 02 del ITE

--- src/data/2024/dataAcuerdos.js
+++ src/data/2024/dataAcuerdos.js
@@ -9,8 +9,25 @@ export const dataAcuerdos2024 = [
     nameDoc:
       "SE APRUEBA PROCESO TÉCNICO OPERATIVO DEL PREP PARA PELO 2023-2024",
     link: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/1.pdf",
-    titleAnexo1: "PROCEOS TÉCNICO OPERATIVO DEL PREP",
+    titleAnexo1: "PROCESO TÉCNICO OPERATIVO DEL PREP",
     pdfAnexo1: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/1.1.pdf",
+  },
+  {
+    id: 2,
+    year: "2024",
+    typeDoc: "ACUERDO",
+    numDoc: "ITE-CG 02-2024",
+    monthDoc: "ENE",
+    nameDoc: "CUMPLIMIENTO A LA SENTENCIA TET-JE-071-2023 Y ACUMULADOS",
+    link: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/2.pdf",
+    titleAnexo1: "LINEAMIENTOS PARA EL REGISTRO DE CANDIDATURAS 2024",
+    pdfAnexo1: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/2.1.pdf",
+    titleAnexo2: "ANEXO UNO",
+    pdfAnexo2: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/2.2.pdf",
+    titleAnexo3: "ANEXO DOS",
+    pdfAnexo3: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/2.3.pdf",
+    titleAnexo4: "ANEXO TRES",
+    pdfAnexo4: "https://itetlax.org.mx/assets/pdf/acuerdos/ITE/2024/2.4.pdf",
   },
 ];
 
