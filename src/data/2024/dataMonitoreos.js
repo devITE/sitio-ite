af23f4428ce062858e591c8cd6d2e182387fd911
PalomaRmMz <p.ramirez.m.8080@gmail.com>
2024-01-17 00:33:07 -0600

    cargados folios, monitoreo y convocatorias

--- src/data/2024/dataMonitoreos.js
+++ src/data/2024/dataMonitoreos.js
@@ -1,1 +1,9 @@
-export const dataMonitoreos2024 = [];
+export const dataMonitoreos2024 = [
+  {
+    id: 1,
+    yearBoletin: "2024",
+    monthBoletin: "ENE",
+    nameBoletin: "Primer Reporte Quincenal",
+    linkBoletin: "https://itetlax.org.mx/assets/pdf/monitoreos/2024/1.pdf",
+  },
+];
