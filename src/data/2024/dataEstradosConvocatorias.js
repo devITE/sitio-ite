1d8aaa3d411b53978eb31819fd05dcf5c8453938
PalomaRmMz <p.ramirez.m.8080@gmail.com>
2024-06-27 09:34:39 -0600

    estradso actualizados

--- src/data/2024/dataEstradosConvocatorias.js
+++ src/data/2024/dataEstradosConvocatorias.js
@@ -5,114 +5,20 @@ export const dataEstrados = [
   //   numCedula: "3257",
   // },
 
-  // bajar el 24 de junio
-  {
-    id: 1,
-    numCedula: "4506",
-  },
-
-  // bajar el 24 de junio
-  {
-    id: 2,
-    numCedula: "4526",
-  },
-  // bajar el 24 de junio
-  {
-    id: 3,
-    numCedula: "4529",
-  },
-  // bajar el 24 de junio
-  {
-    id: 4,
-    numCedula: "4506",
-  },
-
-  // bajar el 24 de junio
-  {
-    id: 5,
-    numCedula: "4535",
-  },
-  // bajar el 24 de junio
-  {
-    id: 6,
-    numCedula: "4536",
-  },
-  // bajar el 24 de junio
-  {
-    id: 7,
-    numCedula: "4550",
-  },
-  // bajar el 24 de junio
-  {
-    id: 8,
-    numCedula: "4561",
-  },
-  // bajar el 24 de junio
-  {
-    id: 9,
-    numCedula: "4563",
-  },
-  // bajar el 24 de junio
-  {
-    id: 10,
-    numCedula: "4575",
-  },
-
-  // bajar el 25 de junio
-  {
-    id: 11,
-    numCedula: "4574",
-  },
-  // bajar el 25 de junio
-  {
-    id: 12,
-    numCedula: "4579",
-  },
-  // bajar el 25 de junio
-  {
-    id: 13,
-    numCedula: "4588",
-  },
-  // bajar el 25 de junio
-  {
-    id: 14,
-    numCedula: "4584",
-  },
-  // bajar el 25 de junio
-  {
-    id: 15,
-    numCedula: "4581",
-  },
-  // bajar el 25 de junio
-  {
-    id: 16,
-    numCedula: "4577",
-  },
-  // bajar el 24 de junio
-  {
-    id: 17,
-    numCedula: "4586",
-  },
-  // bajar el 24 de junio
-  {
-    id: 18,
-    numCedula: "4576",
-  },
-  // bajar el 25 de junio
-  {
-    id: 19,
-    numCedula: "4590",
-  },
-
   // bajar el 26 de junio
   {
-    id: 20,
+    id: 1,
     numCedula: "4625",
   },
   // bajar el 26 de junio
   {
-    id: 21,
+    id: 2,
     numCedula: "4626",
+  },
+  // bajar el 30 de junio
+  {
+    id: 3,
+    numCedula: "4708",
   },
 ];
 
