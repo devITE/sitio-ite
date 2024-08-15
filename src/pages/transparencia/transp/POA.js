c3766b73e8c3ace2fec6014f47641fb2652152e7
PalomaRmMz <p.ramirez.m.8080@gmail.com>
2024-08-15 10:34:41 -0600

    componente POA actualizado

--- src/pages/transparencia/transp/POA.js
+++ src/pages/transparencia/transp/POA.js
@@ -2,18 +2,19 @@ import React, { useEffect } from "react";
 import TitlePages from "../../../layout/TitlePages";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
-import {
-  dataPOA2018,
-  dataPOA2019,
-  dataPOA2020,
-} from "../../../data/dataTransparenciaPOA";
+import { getPOAData } from "../../../data/dataTransparenciaPOA";
 import Breadcrumbs from "../../../layout/Breadcrumbs";
 import "../Transparencia.css";
+
+const baseURL = "https://itetlax.org.mx/assets/pdf/POA/";
+
+const dataYears = [2018, 2019, 2020, 2024];
 
 const POA = () => {
   useEffect(() => {
     document.title = `POA`;
   }, []);
+
   return (
     <>
       <Breadcrumbs
@@ -33,169 +34,71 @@ const POA = () => {
           role="tablist"
           aria-orientation="vertical"
         >
-          <button
-            className="btn btn-ite active"
-            id="vPillsPOA2018Tab"
-            data-bs-toggle="pill"
-            data-bs-target="#vPillsPOA2018"
-            type="button"
-            role="tab"
-            aria-controls="vPillsPOA2018"
-            aria-selected="true"
-          >
-            POAS 2018
-          </button>
-          <button
-            className="btn btn-ite"
-            id="vPillsPOA2019Tab"
-            data-bs-toggle="pill"
-            data-bs-target="#vPillsPOA2019"
-            type="button"
-            role="tab"
-            aria-controls="vPillsPOA2019"
-            aria-selected="false"
-          >
-            POAS 2019
-          </button>
-          <button
-            className="btn btn-ite"
-            id="vPillsPOA2020Tab"
-            data-bs-toggle="pill"
-            data-bs-target="#vPillsPOA2020"
-            type="button"
-            role="tab"
-            aria-controls="vPillsPOA2020"
-            aria-selected="false"
-          >
-            POAS 2020
-          </button>
+          {dataYears.map((year, index) => (
+            <button
+              key={year}
+              className={`btn btn-ite ${index === 0 ? "active" : ""}`}
+              id={`vPillsPOA${year}Tab`}
+              data-bs-toggle="pill"
+              data-bs-target={`#vPillsPOA${year}`}
+              type="button"
+              role="tab"
+              aria-controls={`vPillsPOA${year}`}
+              aria-selected={index === 0 ? "true" : "false"}
+            >
+              POAS {year}
+            </button>
+          ))}
         </div>
         <div className="tab-content w-75" id="vPillsPOASContent">
-          <div
-            className="tab-pane fade show active"
-            id="vPillsPOA2018"
-            role="tabpanel"
-            aria-labelledby="vPillsPOA2018Tab"
-          >
-            <h2 className="text-center mb-3">
-              Programas Operativos Anuales 2018
-            </h2>
-            <table className="table table-striped table-hover w-100">
-              <thead>
-                <tr>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {dataPOA2018.map((poa) => {
-                  return (
-                    <tr key={poa.id}>
-                      <th scope="row">{poa.id}</th>
-                      <td>{poa.title}</td>
-                      <td>
-                        <a
-                          className="btn btn-danger btn-sm"
-                          href={poa.link}
-                          target="_blank"
-                          role="button"
-                          rel="noreferrer"
-                        >
-                          <FontAwesomeIcon icon={faFilePdf} />
-                        </a>
-                      </td>
+          {dataYears.map((year, index) => {
+            const data = getPOAData(year);
+            return (
+              <div
+                key={year}
+                className={`tab-pane fade ${index === 0 ? "show active" : ""}`}
+                id={`vPillsPOA${year}`}
+                role="tabpanel"
+                aria-labelledby={`vPillsPOA${year}Tab`}
+              >
+                <h2 className="text-center mb-3">
+                  Programas Operativos Anuales {year}
+                </h2>
+                <table className="table table-striped table-hover">
+                  <thead>
+                    <tr>
+                      <th scope="col"></th>
+                      <th scope="col"></th>
+                      <th scope="col"></th>
                     </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-
-          <div
-            className="tab-pane fade"
-            id="vPillsPOA2019"
-            role="tabpanel"
-            aria-labelledby="vPillsPOA2019Tab"
-          >
-            <h2 className="text-center mb-3">
-              Programas Operativos Anuales 2019
-            </h2>
-            <table className="table table-striped table-hover">
-              <thead>
-                <tr>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {dataPOA2019.map((poa) => {
-                  return (
-                    <tr key={poa.id}>
-                      <th scope="row">{poa.id}</th>
-                      <td>{poa.title}</td>
-                      <td>
-                        <a
-                          className="btn btn-danger btn-sm"
-                          href={poa.link}
-                          target="_blank"
-                          role="button"
-                          rel="noreferrer"
-                        >
-                          <FontAwesomeIcon icon={faFilePdf} />
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-
-          <div
-            className="tab-pane fade"
-            id="vPillsPOA2020"
-            role="tabpanel"
-            aria-labelledby="vPillsPOA2020Tab"
-          >
-            <h2 className="text-center mb-3">
-              Programas Operativos Anuales 2020
-            </h2>
-            <table className="table table-striped table-hover">
-              <thead>
-                <tr>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                  <th scope="col"></th>
-                </tr>
-              </thead>
-              <tbody>
-                {dataPOA2020.map((poa) => {
-                  return (
-                    <tr key={poa.id}>
-                      <th scope="row">{poa.id}</th>
-                      <td>{poa.title}</td>
-                      <td>
-                        <a
-                          className="btn btn-danger btn-sm"
-                          href={poa.link}
-                          target="_blank"
-                          role="button"
-                          rel="noreferrer"
-                        >
-                          <FontAwesomeIcon icon={faFilePdf} />
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+                  </thead>
+                  <tbody>
+                    {data.map((poa) => (
+                      <tr key={poa.id}>
+                        <th scope="row">{poa.id}</th>
+                        <td>{poa.title}</td>
+                        <td>
+                          <a
+                            className="btn btn-danger btn-sm"
+                            href={`${baseURL}${year}/${poa.id}.pdf`}
+                            target="_blank"
+                            role="button"
+                            rel="noreferrer"
+                          >
+                            <FontAwesomeIcon icon={faFilePdf} />
+                          </a>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
   );
 };
+
 export default POA;
